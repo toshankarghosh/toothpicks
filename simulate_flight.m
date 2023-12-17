c4de6e606c1aa088d61716291aa11c1c12fb3a55
toshankarghosh <34761306+toshankarghosh@users.noreply.github.com>
2023-12-17 18:14:46 +0530

    Update simulate_flight.m

--- simulate_flight.m
+++ simulate_flight.m
@@ -9,24 +9,30 @@ function data = simulate_flight(setup)
 %Set up the integration algorithm
 
 Tspan = setup.Tspan;
+t_check=Tspan(1);
+time2debug=16.52884456410964;
+
+if(t_check>time2debug)
+    disp('hello')
+end
 
 setup.IC.dth = -(setup.p.k/setup.p.gamma)*(setup.IC.th -setup.p.theta_0);
+%is this always true or only if it is coming from contact phases?
+IC = [ setup.IC.xp;  setup.IC.yp;  setup.IC.th;...
+       setup.IC.dxp; setup.IC.dyp; setup.IC.dth ];
 
-IC = [ setup.IC.xc;  setup.IC.yc;  setup.IC.th;...
-       setup.IC.dxc; setup.IC.dyc; setup.IC.dth ];
-
-
+%IC;
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 %                           ODE45                                         %
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
  
-eventFunc       = @(t,z)Event_Flight(t,z,setup);
+eventFunc       = @(t,z)Event_Flight2(t,z,setup);
 options         = odeset(...
                         'RelTol',setup.tol,...
                         'AbsTol',setup.tol,...
                         'Vectorized','on',...
                         'Events',eventFunc,'refine',setup.refine);
-userfun         = @(t,z)dynamics_flight(t,z,setup);
+userfun         = @(t,z)dynamics_flight2(t,z,setup);
 sol             = ode45(userfun,Tspan,IC,options);                         %Run ode45 until termination
 
 
@@ -38,9 +44,8 @@ tspan           = [sol.x(1),sol.x(end)];
 %nTime           = ceil(setup.dataFreq*diff(tspan));
 nTime            =   setup.dataFreq;
 t               = linspace(tspan(1),tspan(2),nTime);                       % create a time array 
-Z               = deval(sol,t);                                            % dval evaluates the solution sol of a differential equation problem at the points contained in t.
-[dZ,Zp, Zs, C]  = dynamics_flight(t,Z,setup);
-
+Zp               = deval(sol,t);                                            % dval evaluates the solution sol of a differential equation problem at the points contained in t.
+[dZ,Zc, Zs, C]  = dynamics_flight2(t,Zp,setup);
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 %                        Formatting                                       %
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
@@ -50,21 +55,21 @@ data.time       = t;
 
 % Values  for the lower base C
 
-data.state.xc   = Z(1,:);
-data.state.yc   = Z(2,:);
-data.state.th   = Z(3,:);
-data.state.dxc  = Z(4,:);
-data.state.dyc  = Z(5,:);
-data.state.dth  = Z(6,:);
+data.state.xc   = Zc(1,:);
+data.state.yc   = Zc(2,:);
+data.state.th   = Zc(3,:);
+data.state.dxc  = Zc(4,:);
+data.state.dyc  = Zc(5,:);
+data.state.dth  = Zc(6,:);
 
 % Values  for the tip P
 
 data.state.xp   = Zp(1,:);
 data.state.yp   = Zp(2,:);
-data.state.pth  = Zp(3,:);
+data.state.th  = Zp(3,:);
 data.state.dxp  = Zp(4,:);
 data.state.dyp  = Zp(5,:);
-data.state.pdth = Zp(6,:);
+data.state.dth = Zp(6,:);
 
 % Values  for the table  S
 
@@ -80,7 +85,7 @@ data.contact.Fy = C(2,:);
 
 data.p          = setup.p;
 
-disp(['The exit condition for Flight is ', num2str(sol.ie)])
+%disp(['The exit condition for Flight is ', num2str(sol.ie)])
 
 %~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%
 %                        State machine                                    %
